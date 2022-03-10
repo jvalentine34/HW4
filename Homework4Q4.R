3a035de0d8f1661fbcf1c58d2b1f9d930105b352
jvalentine34 <jackv@umich.edu>
2022-03-09 19:43:48 -0500

    Homework 4 - Submission

---
+++ Homework4Q4.R
@@ -0,0 +1,20 @@
+library(maps)
+library(mapdata)
+library(ggplot2)
+library(ggrepel)
+library(cowplot)
+library(socviz)
+library(dplyr)
+
+
+cars93 <- MASS::Cars93
+ggplot(cars93, aes(x = Price, y = Fuel.tank.capacity)) +
+  geom_point(color = "grey60") +
+  geom_smooth(se = TRUE, method = "loess", formula = y ~ x, color = "#0072B2") +
+  scale_x_continuous(
+    name = "price (USD)",
+    breaks = c(20, 40, 60),
+    labels = c("$20,000", "$40,000", "$60,000")
+  ) +
+  scale_y_continuous(name = "fuel-tank capacity\n(US gallons)")
+
